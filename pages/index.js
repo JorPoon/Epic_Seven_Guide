7c5eb4305b49873bbb261688eaedae4fb18fe2a1
JorPoon <jormpoon@gmail.com>
2020-03-17 20:16:43 -0400

    continue practicing next.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+
+import Header from '../components/Header';
+
+
+
 export default function Index() {
     return (
       <div>
+        <Header/>
         <p>Hello Next.js</p>
       </div>
     );
