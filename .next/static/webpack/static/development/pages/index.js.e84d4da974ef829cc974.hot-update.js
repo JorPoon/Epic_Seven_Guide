4b41369e416e26abaa6d823408cbd153ab8602c1
JorPoon <jormpoon@gmail.com>
2020-03-18 23:23:02 -0400

    learning how to use shared layouts in next.js

--- .next/static/webpack/static/development/pages/index.js.e84d4da974ef829cc974.hot-update.js
+++ .next/static/webpack/static/development/pages/index.js.e84d4da974ef829cc974.hot-update.js
@@ -12,37 +12,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Index; });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
-/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_MyLayout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/MyLayout */ "./components/MyLayout.js");
 var _jsxFileName = "/Users/jormpoon/Desktop/Personal Projects/epic_seven_guide/pages/index.js";
 
 var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
 
 function Index() {
-  return __jsx("div", {
+  return __jsx(_components_MyLayout__WEBPACK_IMPORTED_MODULE_1__["default"], {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 8
+      lineNumber: 5
     },
     __self: this
-  }, __jsx(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
-    href: "/about",
+  }, __jsx("p", {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 9
-    },
-    __self: this
-  }, __jsx("a", {
-    title: "About Page",
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 10
-    },
-    __self: this
-  }, "About Page")), __jsx("p", {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 12
+      lineNumber: 6
     },
     __self: this
   }, "Hello Next.js"));
@@ -51,4 +36,4 @@ function Index() {
 /***/ })
 
 })
-//# sourceMappingURL=index.js.aea1664a3013fae88ad3.hot-update.js.map
+//# sourceMappingURL=index.js.e84d4da974ef829cc974.hot-update.js.map
