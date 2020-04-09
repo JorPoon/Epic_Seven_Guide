a8c4886e101475ccaff7e5da746015bd3effe520
JorPoon <jormpoon@gmail.com>
2020-04-08 21:43:36 -0400

    renaming some folders and importing header to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-
-
+import Header from "../components/Header"
 const Index = () => {
     return (
         <div>
+            <Header/>
             <h1>Hello From EPIC SEVEN</h1>
         </div>
     )
