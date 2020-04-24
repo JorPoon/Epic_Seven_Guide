5eaa36893a7bf5d6e243310531a2d2f70b6272e3
JorPoon <jormpoon@gmail.com>
2020-04-23 23:13:02 -0400

    adding more to interface details

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fa8fd241b4120c8aec3e";
+/******/ 	var hotCurrentHash = "4706ee6b38f09d245ca6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
