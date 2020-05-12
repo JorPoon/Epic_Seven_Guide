99a216bf6d8f9135adaf9163b03d31458f329b4d
JorPoon <jormpoon@gmail.com>
2020-05-12 18:46:07 -0400

    took out console logs in character folder

--- pages/characters/[id].js
+++ pages/characters/[id].js
@@ -1,4 +1,4 @@
-// import {getAllCharacterIds, getCharacterData} from '../../lib/characters'
+
 
 const Character = (props) => {
   console.log(props)
@@ -11,14 +11,14 @@ const Character = (props) => {
 
 
 export async function getStaticPaths() {
+
   const res = await fetch('https://api.epicsevendb.com/hero')
   const lists = await res.json() 
-  console.log("hello")
-  console.log(lists.results)
+
   const paths = lists.results.map(list => ({
     params: {id: list["_id"]},
   }))
-  //lists.map(list => `/characters/${list.id}`)
+
   return {
     paths,
     fallback: false
@@ -26,9 +26,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
+
   const res = await fetch(`https://api.epicsevendb.com/hero/${params.id}`)
   const characterData = await res.json()
-  console.log(characterData)
+
   return {
     props: {
       characterData
