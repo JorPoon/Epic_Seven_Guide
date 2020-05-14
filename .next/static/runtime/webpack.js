d33e4e312b559d3bdfc43df76bbea866c827534a
JorPoon <jormpoon@gmail.com>
2020-05-14 18:12:15 -0400

    updated package.json and installed new dependcies, clsx and prop-types

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2651f3cdfa09e79e0307";
+/******/ 	var hotCurrentHash = "d1784e6bc99d971f0cef";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
