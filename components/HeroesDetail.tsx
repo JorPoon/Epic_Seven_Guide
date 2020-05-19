a67849d52453d7320bcbbeade626877b4932d1fa
JorPoon <jormpoon@gmail.com>
2020-05-18 23:55:34 -0400

    more styling for cards

--- components/HeroesDetail.tsx
+++ components/HeroesDetail.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent} from 'react';
 import Link from "next/link";
-import { Card, CardMedia, CardContent } from '@material-ui/core'
+import { Card, CardMedia, CardContent, CardHeader } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 
 interface Details {
@@ -13,17 +13,14 @@ interface Details {
 
 const useStyles = makeStyles((theme) => ({
     cardWidth: {
-        minWidth: 225,
+        minWidth: 300,
         margin: 10,
-        alignContent: "center",
-        alignItems: "center",
-        justifyItems: "center",
-        justifyContent: "center"
-
     },
     media: {
-        width: 275,
-        height: 130
+        display: "block",
+        maxWidth: "100%",
+        height: "100%", 
+        paddingTop: '75%', 
     },
     content: {
         background: "orange"
@@ -36,12 +33,13 @@ const HeroesDetail: React.FC <Details> = ({name, attribute, role, assets, rarity
     return (
         <Link href="/hero/[id]" as={`/hero/${name.toLowerCase()}`}>
             <Card className={classes.cardWidth}>
+                <CardHeader title={name}/>
                 <CardMedia
                     className={classes.media}
                     image={assets}
                 />
                 <CardContent className={classes.content} >
-                    <h3>{name}</h3>  
+                     
                     <p> Type: {attribute}</p>
                     <p> Class: {role === "manauser" ? "soul weaver" : role}</p>
                     <p> Rarity: {rarity} stars</p>
