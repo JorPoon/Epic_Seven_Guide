437f9dbfd1fffa4cfa868060eba6185eb23dca00
JorPoon <jormpoon@gmail.com>
2020-05-29 18:13:02 -0400

    took out unneeded modules in navbar

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -1,10 +1,6 @@
 import Link from 'next/link';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-// import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import Zoom from '@material-ui/core/Zoom';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
