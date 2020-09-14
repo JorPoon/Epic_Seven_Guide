a696f671cf99b9a73cb6389c7ed62c062a281746
JorPoon <jormpoon@gmail.com>
2020-09-14 16:35:29 -0400

    trying to implement background imgage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "feb21db757e28c885fae";
+/******/ 	var hotCurrentHash = "42657bf80fc171d88a0b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
