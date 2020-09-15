f04f191c24f094ecda20d9adc536da208f5e396a
JorPoon <jormpoon@gmail.com>
2020-09-14 21:37:40 -0400

    updating next cli

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3b02b0f243251f8459cc";
+/******/ 	var hotCurrentHash = "71dd8b399f3a687319ed";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
